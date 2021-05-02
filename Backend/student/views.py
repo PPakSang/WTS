bd2381521f9bc3df557d998ca68e0ddde78bcb18
PPakSang <ppm5377@naver.com>
2021-05-02 19:41:38 +0900

    initialize, social login test

---
+++ Backend/student/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import redirect, render
+from django.views.generic import *
+import os
+
+# Create your views here.
+
+
+class KakaoLoginTest(View):
+    def get(self,request):
+        REST_API_KEY = '977f61e5094b61a35368e56ded503b70'
+        REDIRECT_URI = 'http://localhost:8000'
+
+        API_HOST = f'https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code'
+
+        return redirect(API_HOST)
