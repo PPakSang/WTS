781ba8a734b81c95e6e6c4cd4eee3027d0943de9
PPakSang <ppm5377@naver.com>
2021-05-12 11:21:38 +0900

    admin modeling

--- Demo/enroll/admin.py
+++ Demo/enroll/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import *
 # Register your models here.
 
-admin.site.register(Student)
+
+
+@admin.register(Student)
+class Admin_model(admin.ModelAdmin):
+    list_display = ['name','is_new','content_size']
+    
+    def content_size(self,obj):
+        return mark_safe(f"<u>{obj.phone_number}</u>") #보안 때문에 mark_safe 로 html 집어넣기 
+    content_size.short_description ='Test'
