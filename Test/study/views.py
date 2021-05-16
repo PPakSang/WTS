10aff677f8cd0918f93ad981050997aeb7cd02dc
PPakSang <ppm5377@naver.com>
2021-05-16 11:23:51 +0900

    고정참여일 템플릿 rendering, 요일변경view

--- Test/study/views.py
+++ Test/study/views.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from django.db.models.query_utils import select_related_descend
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
@@ -69,16 +71,20 @@ def my_study(request): #조회하기
 
     try:
         student = Student.objects.get(user_id = request.user.id)
+        days =[]
+        for i in range(4):
+            days.append(student.changed_day +timedelta(weeks=i))
+
     except :
         return render(request,'student/error.html',{'is_study':False})
 
-    return render(request,'student/detail.html',{'student':student})
+    return render(request,'student/detail.html',{'student':student,'days':days})
     
     
 
 class Enroll(LoginRequiredMixin,generic.CreateView): #등록하기
     model = Student
-    fields=['name','number','level','study','first_day']
+    fields=['name','number','level','first_day']
     template_name = 'student/enroll.html'
     login_url = '/login/'
     
@@ -92,6 +98,7 @@ class Enroll(LoginRequiredMixin,generic.CreateView): #등록하기
     def form_valid(self, form):
         
         form = form.save(commit = False)
+        form.changed_day = form.first_day
         form.user_id = self.request.user.id
         form.save()
 
@@ -113,4 +120,22 @@ def main_view(request): #메인 화면
     else:
         return render(request,'main.html',{'is_student':False})
 
+
+@login_required(login_url='/login/')
+def change_day(request):
+    try:
+        student = Student.objects.get(user_id = request.user.id)
+        if request.method == 'POST':
+            form = Select_day_form(request.POST)
+            if form.is_valid():
+                student.changed_day = form.cleaned_data['changed_day']
+                student.save()
+                return redirect('detail')
+            else:
+                return render(request,'student/change_day.html',{'form':form})
+        else:
+            form = Select_day_form()
+            return render(request,'student/change_day.html',{'form':form})
+    except :
+        return render(request,'student/error.html',{'is_study':False})
 
