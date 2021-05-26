3c1a78ad5a579eb68d1116a9a22361cf2857d214
PPakSang <ppm5377@naver.com>
2021-05-26 12:07:35 +0900

    template version merge

--- Test/study/urls.py
+++ Test/study/urls.py
@@ -5,16 +5,25 @@ from django.utils.timezone import activate
 from .views import *
 
 urlpatterns = [
-    path('',main_view,name='main'),
+    # path('',main_view,name='main'),
     path('signup/',signup,name='signup'),
     path('login/',login,name='login'),
     path('logout/',logout,name='logout'),
     path('detail/',my_study,name='detail'),
+<<<<<<< HEAD
     path('enroll/',Enroll.as_view(),name='enroll'),
     path('change/<int:i>',change_day,name='change'),
     path('wtslist/',only_admin,name='adminpage'),
     path('activate/<slug:uid64>/<slug:token>',activate,name='activate'),
 
     
+=======
+    # path('enroll/',Enroll.as_view(),name='enroll'),
+    path('change/',change_day,name='change'),
+>>>>>>> template_version
 
+
+    # 아래는 템플릿 테스트
+    path('', index, name='index'),
+    path('enroll/', enroll, name='enroll'),
 ]
