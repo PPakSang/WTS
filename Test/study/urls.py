2f62a5bc510e36486977fb6a51cae003be720cf8
PPakSang <ppm5377@naver.com>
2021-05-26 12:09:37 +0900

    url

--- Test/study/urls.py
+++ Test/study/urls.py
@@ -10,17 +10,8 @@ urlpatterns = [
     path('login/',login,name='login'),
     path('logout/',logout,name='logout'),
     path('detail/',my_study,name='detail'),
-<<<<<<< HEAD
-    path('enroll/',Enroll.as_view(),name='enroll'),
-    path('change/<int:i>',change_day,name='change'),
-    path('wtslist/',only_admin,name='adminpage'),
-    path('activate/<slug:uid64>/<slug:token>',activate,name='activate'),
-
-    
-=======
     # path('enroll/',Enroll.as_view(),name='enroll'),
     path('change/',change_day,name='change'),
->>>>>>> template_version
 
 
     # 아래는 템플릿 테스트
