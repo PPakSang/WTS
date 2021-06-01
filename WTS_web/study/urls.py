86da7a5f18db3825fd7c5cf5c270b4a931186639
PPakSang <ppm5377@naver.com>
2021-06-01 13:49:45 +0900

    폴더정리, url정리

---
+++ WTS_web/study/urls.py
@@ -0,0 +1,41 @@
+from collections import namedtuple
+from django.contrib.auth.models import AnonymousUser
+from django.urls import path
+from django.utils.timezone import activate
+from .views import *
+
+urlpatterns = [
+    # path('',main_view,name='main'),
+    # path('signup/',signup,name='signup'),
+    # path('login/',login,name='login'),
+    #path('detail/',my_study,name='detail'),
+    # path('enroll/',Enroll.as_view(),name='enroll'),
+    # path('change/',change_day,name='change'),
+    
+
+
+    #main
+    path('', index, name='index'),
+
+    #user
+    path('user/signup/', signup_hw, name='signup'),
+    path('user/login/', login_hw, name='login'),
+    path('user/findid/',find_id,name='find_id'),
+    path('user/findpw/',find_pw,name='find_pw'),
+    path('user/resetpw/<uid64>/<token>',reset_pw,name='reset_pw'),
+    path('user/checkemail/', checkemail, name='check_email'),
+    path('user/activate/<uid64>/<token>',activate,name='activate'),
+    path('user/logout/',logout,name='logout'),
+
+    #category
+    path('category/inquire/', inquire, name='inquire'),
+    path('category/enroll/', enroll, name='enroll'),
+    path('category/change/', change, name='change'),
+    path('category/faq/', faq_view, name='faq'),
+
+    #policy
+
+    #admin
+    path('onlyadmin/<option>', only_admin, name='adminpage'),
+    
+]
