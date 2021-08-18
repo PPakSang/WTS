daf8ae40c8c5972af79f13b58501f1bb38ea7c63
HyeW <qorgp13@naver.com>
2021-08-18 19:13:38 +0900

    간편로그인

--- WTS_web/study/urls.py
+++ WTS_web/study/urls.py
@@ -11,48 +11,50 @@ urlpatterns = [
     # path('',main_view,name='main'),
     # path('signup/',signup,name='signup'),
     # path('login/',login,name='login'),
-    #path('detail/',my_study,name='detail'),
+    # path('detail/',my_study,name='detail'),
     # path('enroll/',Enroll.as_view(),name='enroll'),
     # path('change/',change_day,name='change'),
-    
 
 
-    #main
+
+    # main
     path('', index, name='index'),
 
-    #user
+    # user
     path('user/signup/', signup_hw, name='signup'),
+    path('user/signupsns/', signup_sns, name='signup_sns'),
     path('user/login/', login_hw, name='login'),
-    path('user/findid/',find_id,name='find_id'),
-    path('user/findpw/',find_pw,name='find_pw'),
-    path('user/resetpw/<uid64>/<token>',reset_pw,name='reset_pw'),
+    path('user/findid/', find_id, name='find_id'),
+    path('user/findpw/', find_pw, name='find_pw'),
+    path('user/resetpw/<uid64>/<token>', reset_pw, name='reset_pw'),
     path('user/checkemail/', checkemail, name='check_email'),
-    path('user/activate/<uid64>/<token>',activate,name='activate'),
-    path('user/logout/',logout,name='logout'),
+    path('user/activate/<uid64>/<token>', activate, name='activate'),
+    path('user/logout/', logout, name='logout'),
 
-    path('user/isduplicated/',is_duplicated,name='is_duplicated'),
-    path('user/resend/<email>',re_send,name='re_send'),
+    path('user/isduplicated/', is_duplicated, name='is_duplicated'),
+    path('user/resend/<email>', re_send, name='re_send'),
 
-    #category
+    # category
     path('category/inquire/', inquire, name='inquire'),
     path('category/enroll/', enroll, name='enroll'),
     path('category/change/', change, name='change'),
     path('category/faq/', faq_view, name='faq'),
 
 
-    #policy
+    # policy
 
-    #admin
+    # admin
     path('onlyadmin/<option>', only_admin, name='adminpage'),
-    path('deposit_page/<option>',deposit_view,name='deposit_view'),
-    path('checkdeposit/<option>/<user_id>/<way>',check_deposit,name='check_deposit'),
-    path('checkin/<user_id>',check_in,name='check_in'),
+    path('deposit_page/<option>', deposit_view, name='deposit_view'),
+    path('checkdeposit/<option>/<user_id>/<way>',
+         check_deposit, name='check_deposit'),
+    path('checkin/<user_id>', check_in, name='check_in'),
 
-    #error
-    path('error/',testError,name='error'),
+    # error
+    path('error/', testError, name='error'),
 
-    path('uploadimg/',Img_update_view.as_view()),
-    path('deleteimg/<pk>',deleteimg)
+    path('uploadimg/', Img_update_view.as_view()),
+    path('deleteimg/<pk>', deleteimg)
 ]
 # +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
